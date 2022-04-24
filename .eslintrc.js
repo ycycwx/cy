27180d54e03dcaa81b191904987e66e380719519
ycycwx <yytcjcy@gmail.com>
2022-04-25 02:10:34 +0800

    feat: add @yotsubacy/config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-    extends: [
-        '@ecomfe/eslint-config/strict',
-        '@ecomfe/eslint-config/typescript/strict',
-    ],
+    extends: [require.resolve('@yotsubacy/config/eslint/node'), require.resolve('@yotsubacy/config/eslint/typescript')],
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+        },
+    },
 };
